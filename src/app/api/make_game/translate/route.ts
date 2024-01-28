bf61df704dac87130926ad3ce45465fee83aa26c
shaunfrancis <shaunfrancis@gmx.com>
2024-01-28 18:26:49 +0000

    Enable translation API

--- src/app/api/make_game/translate/route.ts
+++ src/app/api/make_game/translate/route.ts
@@ -17,15 +17,14 @@ export async function POST(request: NextRequest) : Promise<NextResponse> {
         const slack = new SlackClient(process.env.SLACK_TOKEN);
         await slack.chat.postMessage({ channel: '#lyriclocale', text: 'Translation API called.' });
 
-        //const translate = new GoogleTranslate.Translate();
+        const translate = new GoogleTranslate.Translate();
         const langs : Language[] = Languages.filter(l => l.difficulty == 1).sort(() => Math.random() - Math.random()).slice(0, 1);
         langs.push(...Languages.filter(l => !l.difficulty).sort(() => Math.random() - Math.random()).slice(0, 5));
         
         const clues : any[] = [];
 
         for(let index = 0; index < langs.length; index++){
-            //const translatedLyrics = await translate.translate(lyrics, langs[index].code);
-            const translatedLyrics : [string, {data:{translations:[{detectedSourceLanguage:string}]}}] = ["Translated lyrics", {data:{translations:[{detectedSourceLanguage:"en"}]}}]; //dummy data
+            const translatedLyrics = await translate.translate(lyrics, langs[index].code);
 
             if(translatedLyrics[1].data.translations[0].detectedSourceLanguage != "en") return rejectSong(song, 3);
 
