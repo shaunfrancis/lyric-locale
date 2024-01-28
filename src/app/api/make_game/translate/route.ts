d57b9b3cb0aab81f5faa6e0c42aef55772758cb7
shaunfrancis <shaunfrancis@gmx.com>
2024-01-28 16:50:40 +0000

    Add Slack notifications to translate API

--- src/app/api/make_game/translate/route.ts
+++ src/app/api/make_game/translate/route.ts
@@ -1,6 +1,7 @@
 export const dynamic = "force-dynamic";
 
 import { v2 as GoogleTranslate } from '@google-cloud/translate';
+import { WebClient as SlackClient } from '@slack/web-api';
 import Language from '@/types/Language';
 import { Languages } from '@/constants/Languages';
 import { NextRequest, NextResponse } from 'next/server';
@@ -10,9 +11,12 @@ import authenticate from '@/lib/authenticate';
 export async function POST(request: NextRequest) : Promise<NextResponse> {
     if(!authenticate(request)) return NextResponse.json( {error : "UNAUTHORISED"}, {status: 401} );
     
-    const song = await request.json();
+    const {song, lyrics} = await request.json();
 
     try{
+        const slack = new SlackClient(process.env.SLACK_TOKEN);
+        await slack.chat.postMessage({ channel: '#lyriclocale', text: 'Translation API called.' });
+
         //const translate = new GoogleTranslate.Translate();
         const langs : Language[] = Languages.filter(l => l.difficulty == 1).sort(() => Math.random() - Math.random()).slice(0, 1);
         langs.push(...Languages.filter(l => !l.difficulty).sort(() => Math.random() - Math.random()).slice(0, 5));
