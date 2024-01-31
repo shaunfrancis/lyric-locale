c743ec9fc98a77ce0c22765ba2d163068a31a4cb
shaunfrancis <shaunfrancis@gmx.com>
2024-01-31 23:34:51 +0000

    Streak styling

--- src/components/StatsContainer.tsx
+++ src/components/StatsContainer.tsx
@@ -8,14 +8,28 @@ export default function StatsContainer(
     {statsPopup, streak, bestStreak} : 
     {statsPopup : MutableRefObject<HTMLDivElement | null>, streak : number, bestStreak : number}
 ){
+
+    const streakComments = ["C'est la vie...", "Que será, será", "Molto bene", "Très bien !"];
+    const bestStreakComments = ["Nul points", "Bon courage !", "Fantastisch", "¡Muy bien!"];
+
     return (
         <Popup popupRef={statsPopup}>
             <h1>
                 <img src="/streak.svg" alt="" style={{height:"35px"}}/>
                 <span>Streak</span>
             </h1>
-            <p>Your current daily success streak is {streak}.</p>
-            <p>Your best daily success streak is {Math.max(streak, bestStreak)}.</p>
+            <div className="two-column">
+                <div className="column">
+                    <h2>Current Streak</h2>
+                    <div className="streak-score">{streak}</div>
+                    <p>{streakComments[Math.min(streakComments.length - 1, streak)]}</p>
+                </div>
+                <div className="column">
+                    <h2>Best Streak</h2>
+                    <div className="streak-score">{Math.max(streak, bestStreak)}</div>
+                    <p>{bestStreakComments[Math.min(bestStreakComments.length - 1, Math.max(streak, bestStreak))]}</p>
+                </div>
+            </div>
         </Popup>
     )
 }
