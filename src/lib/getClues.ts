5c671665b0945e6fd38db1b3b1cdcac1c2d945e8
shaunfrancis <shaunfrancis@gmx.com>
2024-02-10 15:51:02 +0000

    Transition away from Postgres for clues

--- src/lib/getClues.ts
+++ src/lib/getClues.ts
@@ -1,19 +1,26 @@
 import { unstable_noStore as noStore } from 'next/cache';
 import { DefaultLanguage, Languages } from '@/constants/Languages';
 import Clue from '@/types/Clue';
-import { sql } from '@vercel/postgres';
 
 export default async function postgresGetClues(id : number){
     noStore();
 
-    const { rows } = await sql`SELECT level, language, lyrics FROM clues WHERE game_id = ${id} ORDER BY level ASC`;
+    const failedClues : Clue[] = Array(6).fill({level: -1, lyrics: "Missing Data", language: DefaultLanguage});
 
-    if(rows.length != 6) return Array(6).fill({level: -1, lyrics: "Missing Data", language: DefaultLanguage});
-    
-    rows.forEach( clue => {
-        const language = Languages.find( c => c.code == clue.language ) || DefaultLanguage;
-        clue.language = language;
-    });
+    try{
+        const request = await fetch(process.env.TS_API_URL + "/clues.php?id=" + id);
+        const rows : any[] = await request.json();
 
-    return rows as Clue[];
+        if(rows.length != 6) return failedClues;
+        
+        rows.forEach( clue => {
+            const language = Languages.find( c => c.code == clue.language ) || DefaultLanguage;
+            clue.language = language;
+        });
+
+        return rows as Clue[];
+    }
+    catch(error){
+        return failedClues;
+    }
 }
